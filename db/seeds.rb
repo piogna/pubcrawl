d4c5197fee408978d3200965edc07a7b78dcc9b5
Hanna Jones <jones.hanna.m@gmail.com>
2016-01-27 23:44:57 +0000

    created show page for each crawl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 require 'faker'
 
+crawl_1 = Crawl.create!(
+  name: "Cool Crawl"
+)
+
+crawl_2 = Crawl.create!(
+  name: "Sick Crawl"
+)
+
 bar_1 = Bar.create!(
   name: "Sneaky Dee's",
   address: "431 College Street",
   city: "Toronto",
   postal_code: "M5T 1T1",
   crawl_position: 1,
-  crawl_id: 1
+  crawl: crawl_1
 )
 
 bar_2 = Bar.create!(
@@ -14,8 +22,8 @@ bar_2 = Bar.create!(
   address: "609 King St W",
   city: "Toronto",
   postal_code: "M5V 1M5",
-  crawl_id: 2,
-  crawl_position: 1
+  crawl_position: 1,
+  crawl: crawl_2
 )
 
 bar_3 = Bar.create!(
@@ -23,8 +31,8 @@ bar_3 = Bar.create!(
   address: "391 King St W",
   city: "Toronto",
   postal_code: "M5V 1K1",
-  crawl_id: 2,
-  crawl_position: 2
+  crawl_position: 2,
+  crawl: crawl_2
 )
 
 bar_4 = Bar.create!(
@@ -32,8 +40,8 @@ bar_4 = Bar.create!(
   address: "370 Queen St W",
   city: "Toronto",
   postal_code: "M5V 2A2",
-  crawl_id: 2,
-  crawl_position: 3
+  crawl_position: 3,
+  crawl: crawl_2
 )
 
 bar_5 = Bar.create!(
@@ -41,8 +49,8 @@ bar_5 = Bar.create!(
   address: "1265 Dundas St W",
   city: "Toronto",
   postal_code: "M6J 1X6",
-  crawl_id: 1,
-  crawl_position: 2
+  crawl_position: 2,
+  crawl: crawl_1
 )
 
 bar_6 = Bar.create!(
@@ -50,14 +58,6 @@ bar_6 = Bar.create!(
   address: "1665 Dundas St W",
   city: "Toronto",
   postal_code: "M6K 1V2",
-  crawl_id: 1,
-  crawl_position: 3
+  crawl_position: 3,
+  crawl: crawl_1
 )
-
-2.times do
-  name = Faker::App.name
-  Crawl.create!(
-    name: name
-  )
-end
-
