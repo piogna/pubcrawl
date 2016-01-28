4c7af7b00881ad9b1fdc08022c643ec8f478e1d3
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-01-28 16:25:02 +0000

    crawl/id/add_bar works. also lists bars already added to crawl

--- app/actions.rb
+++ app/actions.rb
@@ -4,26 +4,36 @@ get '/' do
   erb :index
 end
 
-# get '/crawl/:id' do
-#   erb :'crawl/:id'
-# end
+get '/crawl/:id' do
+  erb :'crawl/:id'
+end
 
-# get '/crawl/new' do
-#   @bar = Bar.new
-#   erb :'crawl/new'
-# end
 
 post '/crawl/new' do
   @crawl = Crawl.new(
     name: params[:name]
     )
   @crawl.save
+  binding.pry
   redirect "/crawl/#{@crawl.id}/add_bar"
 end
 
 get '/crawl/:id/add_bar' do
-  @crawl = Crawl.find_by params[:id]
-
+  @crawl = Crawl.find(params[:id])
   @bar = Bar.new
+  binding.pry
   erb :'crawl/new'
+end
+
+post '/crawl/:id/add_bar' do
+  @crawl = Crawl.find(params[:id])
+  @bar = Bar.new(
+    name:     params[:name],
+    address:  params[:address],
+    city:     params[:city],
+    crawl_id: @crawl.id
+    )
+  binding.pry
+  @bar.save
+  redirect "/crawl/#{@crawl.id}/add_bar"
 end
