f72504dcffbb8fbbe1f9278581f6cc0d53d6c504
Hanna Jones <jones.hanna.m@gmail.com>
2016-01-28 22:25:57 +0000

    implemented container-fluid div and created table for show page

--- app/actions.rb
+++ app/actions.rb
@@ -48,3 +48,44 @@ post '/crawl/:id/add_bar' do
   @bar.save
   redirect "/crawl/#{@crawl.id}/add_bar"
 end
+
+get '/crawl/done/:id' do
+  @crawl = Crawl.find(params[:id])
+  @bars = Bar.where(crawl_id: params[:id]).order(crawl_position: :asc)
+  @bar = Bar.new(
+    name:     params[:name],
+    address:  params[:address],
+    city:     params[:city],
+    crawl_id: @crawl.id
+    )
+  uri_string = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@bar.address},#{@bar.city}&key=AIzaSyBX6s8EgaN-Pv8axPpoZvHYY4xL4fRHnck"
+  uri_string.gsub!(" ", "+")
+  uri = URI(uri_string)
+  response = Net::HTTP.get uri
+  location = JSON.parse response
+  @bar.latitude = location["results"][0]["geometry"]["location"]["lat"].to_s
+  @bar.longitude = location["results"][0]["geometry"]["location"]["lng"].to_s
+  @bar.save
+  erb :'crawl/show'
+end
+
+# post '/crawl/done/:id' do
+#   @crawl = Crawl.find(params[:id])
+#   @bar = Bar.new(
+#     name:     params[:name],
+#     address:  params[:address],
+#     city:     params[:city],
+#     crawl_id: @crawl.id
+#   )
+#   uri_string = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@bar.address},#{@bar.city}&key=AIzaSyBX6s8EgaN-Pv8axPpoZvHYY4xL4fRHnck"
+#   uri_string.gsub!(" ", "+")
+#   uri = URI(uri_string)
+#   response = Net::HTTP.get uri
+#   location = JSON.parse response
+#   @bar.latitude = location["results"][0]["geometry"]["location"]["lat"].to_s
+#   @bar.longitude = location["results"][0]["geometry"]["location"]["lng"].to_s
+#   @bar.save
+#   redirect "/crawl/#{@crawl.id}"
+# end
+
+
