eafefe9170102475817262ef3ed1565bb031bbb6
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-01-30 00:41:41 +0000

    add validations to User class and add default values to new modals

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, :last_name, presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true
+
 end
